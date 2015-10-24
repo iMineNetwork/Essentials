83d833e77fd6aa07baa340c11a6b59fd4627528c
MakerTim <makertim@outlook.com>
2015-10-24 19:17:37 +0200

    auto /git on login

--- src/nl/MakerTim/HubEssentials/BukkitListener.java
+++ src/nl/MakerTim/HubEssentials/BukkitListener.java
@@ -1,14 +1,24 @@
 package nl.MakerTim.HubEssentials;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent.Result;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class BukkitListener implements Listener {
 
+	@EventHandler
 	public void onJoin(AsyncPlayerPreLoginEvent event) {
 		if (!BukkitStarter.isDev(event.getUniqueId()) && BukkitStarter.plugin.devMode) {
 			event.disallow(Result.KICK_FULL, "Server is now in dev mode.");
+		}
+	}
+
+	@EventHandler
+	public void onJoining(PlayerJoinEvent pje) {
+		if (pje.getPlayer().isOp() || BukkitStarter.isDev(pje.getPlayer().getUniqueId())) {
+			pje.getPlayer().performCommand("git");
 		}
 	}
 }
