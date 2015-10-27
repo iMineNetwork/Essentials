a8acae4eb5089758c6acf7a0bffadca75f186346
MakerTim <makertim@outlook.com>
2015-10-27 19:30:34 +0100

    added wessel + database start

--- src/nl/MakerTim/HubEssentials/BukkitStarter.java
+++ src/nl/MakerTim/HubEssentials/BukkitStarter.java
@@ -15,14 +15,17 @@ public class BukkitStarter extends JavaPlugin {
 			UUID.fromString("b524c508-9f1e-4cef-965a-4a7b9017055c"), // Sansko1337
 			UUID.fromString("9bbc05fa-f36c-401a-8c23-c57f5ba5c5ee"), // Sansko1338
 			UUID.fromString("4697c45f-e9fe-4a0a-abeb-8783d6ac7ca6"), // Beauseant
+			UUID.fromString("203fe4b3-928f-4780-8067-6d557fdcc153"), // wsslfnstr
 	};
 	public static BukkitStarter plugin;
 
+	private DatabaseManager dm;
 	public boolean devMode = false;
 
 	@Override
 	public void onEnable() {
 		plugin = this;
+		dm = new DatabaseManager(Credentials.getDatabase(), Credentials.getUsername(), Credentials.getPassword());
 		getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
 		Bukkit.getPluginManager().registerEvents(new BukkitListener(), this);
 	}
@@ -35,6 +38,10 @@ public class BukkitStarter extends JavaPlugin {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		return CommandHandler.onCommand(sender, command.getName(), args);
+	}
+
+	public DatabaseManager getDm() {
+		return dm;
 	}
 
 	public static boolean isDev(UUID check) {
