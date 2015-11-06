560b5085040367d91dffe144ea3279e9335a73c5
MakerTim <makertim@outlook.com>
2015-11-06 20:02:51 +0100

    shop & bugs auto awnser message

--- src/nl/MakerTim/HubEssentials/BukkitListener.java
+++ src/nl/MakerTim/HubEssentials/BukkitListener.java
@@ -1,12 +1,17 @@
 package nl.MakerTim.HubEssentials;
 
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent.Result;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class BukkitListener implements Listener {
+
+	private static final String[] SHOP_FORMAT = { "shop", "duur", "kost", "kopen", "whitelist", "vip" };
+	private static final String[] BUGS_FORMAT = { "help", "bug", "fout", "error" };
 
 	@EventHandler
 	public void onJoin(AsyncPlayerPreLoginEvent event) {
@@ -19,6 +24,31 @@ public class BukkitListener implements Listener {
 	public void joinDeop(PlayerJoinEvent pje) {
 		if (pje.getPlayer().isOp()) {
 			pje.getPlayer().setOp(false);
+		}
+	}
+
+	@EventHandler
+	public void chat(AsyncPlayerChatEvent apce) {
+		String mssg = apce.getMessage();
+		boolean sendMssg = false;
+		for (String shp : SHOP_FORMAT) {
+			if (mssg.toLowerCase().contains(shp)) {
+				sendMssg = true;
+			}
+		}
+		if (sendMssg) {
+			apce.getPlayer().sendMessage(ChatColor.GOLD + ChatColor.BOLD.toString() + "Do you like to purchase one of our fine VIP ranks or a whitelist? Click here -> "
+					+ ChatColor.BLUE + "shop.iMine.nl");
+		}
+		sendMssg = false;
+		for (String bgs : BUGS_FORMAT) {
+			if (mssg.toLowerCase().contains(bgs)) {
+				sendMssg = true;
+			}
+		}
+		if (sendMssg) {
+			apce.getPlayer().sendMessage(ChatColor.RED + ChatColor.BOLD.toString() + "Found a bug? Report it to -> "
+					+ ChatColor.BLUE + "bugs.iMine.nl");
 		}
 	}
 
