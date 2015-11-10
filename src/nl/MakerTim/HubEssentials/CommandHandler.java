5e2ef654f643f0e3b229dd8256cabeefc9b6f79c
MakerTim <makertim@outlook.com>
2015-11-10 22:14:45 +0100

    Yaw & Pitch fix

--- src/nl/MakerTim/HubEssentials/CommandHandler.java
+++ src/nl/MakerTim/HubEssentials/CommandHandler.java
@@ -208,17 +208,19 @@ public class CommandHandler {
 				try {
 					if (faultArg == 0 && args.length == 4) {
 						Player target = getPlayer(args[0]);
-						target.teleport(new Location(target.getWorld(), coords[1], coords[2], coords[3]));
+						target.teleport(new Location(target.getWorld(), coords[1], coords[2], coords[3],
+								target.getLocation().getYaw(), target.getLocation().getPitch()));
 						sender.sendMessage(ChatColor.GOLD + "Teleporting...");
 					} else if (faultArg < 0) {
 						if (sender instanceof Player) {
 							Player who = (Player) sender;
 							if (args.length == 3) {
-								who.teleport(new Location(who.getWorld(), coords[0], coords[1], coords[2]));
+								who.teleport(new Location(who.getWorld(), coords[0], coords[1], coords[2],
+										who.getLocation().getYaw(), who.getLocation().getPitch()));
 								sender.sendMessage(ChatColor.GOLD + "Teleporting...");
 							} else {
 								who.teleport(new Location(Bukkit.getWorlds().get((int) coords[3]), coords[0], coords[1],
-										coords[2]));
+										coords[2], who.getLocation().getYaw(), who.getLocation().getPitch()));
 								sender.sendMessage(ChatColor.GOLD + "Teleporting...");
 							}
 						} else {
