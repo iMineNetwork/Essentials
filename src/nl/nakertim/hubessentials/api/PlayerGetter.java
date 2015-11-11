3d348ef1ff38a669c4642b60364677ad0981376b
MakerTim <makertim@outlook.com>
2015-11-11 20:25:35 +0100

    API update!

---
+++ src/nl/nakertim/hubessentials/api/PlayerGetter.java
@@ -0,0 +1,93 @@
+package nl.nakertim.hubessentials.api;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.annotation.CheckForNull;
+
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+
+public class PlayerGetter {
+
+	@CheckForNull
+	public static OfflinePlayer get(String name) {
+		OfflinePlayer ret = getOnline(name);
+		if (ret == null) {
+			ret = getOflline(name);
+		}
+		return ret;
+	}
+
+	public static List<String> getAllNames(String name) {
+		List<String> ret = new ArrayList<>();
+		ret.addAll(getAllOnlineNames(name));
+		ret.addAll(getAllOflineNames(name));
+		return ret;
+	}
+
+	public static List<OfflinePlayer> getAll(String name) {
+		List<OfflinePlayer> ret = new ArrayList<>();
+		ret.addAll(getAllOnline(name));
+		ret.addAll(getAllOffline(name));
+		return ret;
+	}
+
+	@CheckForNull
+	public static Player getOnline(String name) {
+		List<Player> ret = getAllOnline(name);
+		if (ret.isEmpty()) {
+			return null;
+		}
+		return ret.get(0);
+	}
+
+	public static List<String> getAllOnlineNames(String name) {
+		List<String> ret = new ArrayList<>();
+		for (Player pl : getAllOnline(name)) {
+			ret.add(pl.getName());
+		}
+		return ret;
+	}
+
+	public static List<Player> getAllOnline(String name) {
+		List<Player> ret = new ArrayList<>();
+		for (Player pl : Bukkit.getOnlinePlayers()) {
+			if (pl.getName().toLowerCase().contains(name.toLowerCase())) {
+				ret.add(pl);
+			}
+		}
+		Collections.sort(ret, new PlayerSearchSorter(name));
+		return ret;
+	}
+
+	@CheckForNull
+	public static OfflinePlayer getOflline(String name) {
+		List<OfflinePlayer> ret = getAllOffline(name);
+		if (ret.isEmpty()) {
+			return null;
+		}
+		return ret.get(0);
+	}
+
+	public static List<String> getAllOflineNames(String name) {
+		List<String> ret = new ArrayList<>();
+		for (OfflinePlayer pl : getAllOffline(name)) {
+			ret.add(pl.getName());
+		}
+		return ret;
+	}
+
+	public static List<OfflinePlayer> getAllOffline(String name) {
+		List<OfflinePlayer> ret = new ArrayList<>();
+		for (OfflinePlayer pl : Bukkit.getOfflinePlayers()) {
+			if (pl.getName().toLowerCase().contains(name.toLowerCase())) {
+				ret.add(pl);
+			}
+		}
+		Collections.sort(ret, new PlayerSearchSorter(name));
+		return ret;
+	}
+}
---
+++ src/nl/nakertim/hubessentials/api/PlayerSearchSorter.java
@@ -0,0 +1,23 @@
+package nl.nakertim.hubessentials.api;
+
+import java.util.Comparator;
+
+import org.bukkit.OfflinePlayer;
+
+public class PlayerSearchSorter implements Comparator<OfflinePlayer> {
+
+	private final String toFind;
+
+	public PlayerSearchSorter(String searchingFor) {
+		toFind = searchingFor;
+	}
+
+	@Override
+	public int compare(OfflinePlayer p1, OfflinePlayer p2) {
+		if (toFind.isEmpty()) {
+			return p1.getName().toLowerCase().compareTo(p2.getName().toLowerCase());
+		}
+		return p1.getName().toLowerCase().indexOf(toFind.toLowerCase())
+				- p2.getName().toLowerCase().indexOf(toFind.toLowerCase());
+	}
+}
