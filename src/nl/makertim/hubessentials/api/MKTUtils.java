3a10faa8a96397652f838ab270575e80a48cf05d
MakerTim <makertim@outlook.com>
2015-12-04 23:13:32 +0100

    Unifersal MktUtils class
    added banmanager en hubessentials

---
+++ src/nl/makertim/hubessentials/api/MKTUtils.java
@@ -0,0 +1,112 @@
+package nl.makertim.hubessentials.api;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.entity.Player;
+
+import nl.makertim.hubessentials.BukkitStarter;
+
+public class MKTUtils {
+	private static Map<Character, Integer> timeDic;
+	private static final long MINUTES_IN_SECONDS = 60;
+	private static final long HOURS_IN_SECONDS = MINUTES_IN_SECONDS * 60;
+	private static final long DAYS_IN_SECONDS = HOURS_IN_SECONDS * 24;
+	private static final long WEEKS_IN_SECONDS = DAYS_IN_SECONDS * 7;
+	private static final long MONTHS_IN_SECONDS = DAYS_IN_SECONDS * 28;
+	private static final long YEARS_IN_SECONDS = DAYS_IN_SECONDS * 356;
+
+	private static int getFlag(char c) {
+		if (timeDic == null) {
+			timeDic = new HashMap<>();
+			timeDic.put('s', Calendar.SECOND);
+			timeDic.put('m', Calendar.MINUTE);
+			timeDic.put('h', Calendar.HOUR);
+			timeDic.put('u', Calendar.HOUR);
+			timeDic.put('d', Calendar.DAY_OF_MONTH);
+			timeDic.put('j', Calendar.YEAR);
+			timeDic.put('y', Calendar.YEAR);
+		}
+		return timeDic.get(c);
+	}
+
+	public static Date fromShortString(String str) {
+		Calendar c = Calendar.getInstance();
+		c.setTime(new Date(0));
+		try {
+			char timeChar = str.charAt(str.length() - 1);
+			if (timeChar == 'w') {
+				c.add(getFlag('d'), Integer.valueOf(str.substring(0, str.length() - 1)) * 7);
+			} else {
+				c.add(getFlag(timeChar), Integer.valueOf(str.substring(0, str.length() - 1)));
+			}
+		} catch (Exception ex) {
+			System.err.println(ex.getMessage());
+		}
+		return c.getTime();
+	}
+
+	public static Date combine(Date first, Date toAdd) {
+		return new Date(first.getTime() + toAdd.getTime());
+	}
+
+	public static String timeUntilNow(Date until) {
+		String ret = "";
+		long secondsBetween = (long) Math.ceil(((until.getTime() - new Date().getTime()) / 1000));
+		double minus = 0;
+		if (secondsBetween >= YEARS_IN_SECONDS) {
+			minus = Math.floor(secondsBetween / YEARS_IN_SECONDS);
+			secondsBetween -= (long) minus * YEARS_IN_SECONDS;
+			ret += (int) minus + " years, ";
+		}
+		if (secondsBetween >= MONTHS_IN_SECONDS) {
+			minus = Math.floor(secondsBetween / MONTHS_IN_SECONDS);
+			secondsBetween -= (long) minus * MONTHS_IN_SECONDS;
+			ret += (int) minus + " months, ";
+		}
+		if (secondsBetween >= WEEKS_IN_SECONDS) {
+			minus = Math.floor(secondsBetween / WEEKS_IN_SECONDS);
+			secondsBetween -= (long) minus * WEEKS_IN_SECONDS;
+			ret += (int) minus + " weeks, ";
+		}
+		if (secondsBetween >= DAYS_IN_SECONDS) {
+			minus = Math.floor(secondsBetween / DAYS_IN_SECONDS);
+			secondsBetween -= (long) minus * DAYS_IN_SECONDS;
+			ret += (int) minus + " days, ";
+		}
+		if (secondsBetween >= HOURS_IN_SECONDS) {
+			minus = Math.floor(secondsBetween / HOURS_IN_SECONDS);
+			secondsBetween -= (long) minus * HOURS_IN_SECONDS;
+			ret += (int) minus + " hours, ";
+		}
+		if (secondsBetween >= MINUTES_IN_SECONDS) {
+			minus = Math.floor(secondsBetween / MINUTES_IN_SECONDS);
+			secondsBetween -= (long) minus * MINUTES_IN_SECONDS;
+			ret += (int) minus + " minutes, ";
+		}
+		if (ret.endsWith(", ")) {
+			ret = ret.substring(0, ret.length() - 2);
+		}
+		return ret;
+	}
+
+	public static void sendPlayerToServer(final Player pl, final String serverID) {
+		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			DataOutputStream dos = new DataOutputStream(baos);
+			dos.writeUTF("Connect");
+			dos.writeUTF(serverID);
+			pl.sendPluginMessage(BukkitStarter.plugin, "BungeeCord", baos.toByteArray());
+			baos.close();
+			dos.close();
+		} catch (Exception ex) {
+			System.err.println("Player " + pl.getName() + " has not been sended to " + serverID + " because of "
+					+ ex.getMessage());
+			pl.sendMessage("You " + " have not been sended to " + serverID + " because of " + ex.getMessage());
+		}
+	}
+}
